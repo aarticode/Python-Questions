226629305a2d49c196ebcb06cdb2b3fdcaa1c858
aarticode <aartisingh1117@gmail.com git config --global user.email>
2023-08-27 18:31:23 +0530

    reverse-string

--- reversestring.py
+++ reversestring.py
@@ -3,3 +3,15 @@
 fname=input("enter first name: ")
 lname=input("enter last name: ")
 print("Hello "+lname+" "+fname)
+
+
+#Write a Python function that takes a string as input and returns
+# the reverse of that string. For example, if the input is "Hello, World!",
+# the function should return "!dlroW ,olleH".
+def reverse(string):
+    n=""
+    for b in string:
+        print (b)
+        n=b+n
+    return n
+print (reverse("AARTI,chouhan"))
