67f968de9de7bbf3b2a94ca4cb1953f51bb99775
aarticode <aartisingh1117@gmail.com>
2023-09-03 18:53:25 +0530

    x

---
+++ variables.py
@@ -0,0 +1,24 @@
+#change the global variable with the global keyword
+x = 100
+
+def myfunc():
+  x = 100
+
+
+print(x)
+
+#python output varibale
+x = 10
+y = 15
+z = 20
+print(x, y, z)
+
+#local of python varibale
+
+#loacl scope
+def demo():
+    a=100
+    print(a)
+
+    #call
+demo()
