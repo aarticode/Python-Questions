275a4f7363b04bc252c2fa4780cc5c62bf563440
aarticode <aartisingh1117@gmail.com>
2023-09-06 21:28:15 +0530

    if_else

--- if_else_statment.py
+++ if_else_statment.py
@@ -4,3 +4,11 @@ a = 330
 b = 330
 
 print("A") if a > b else print("=") if a == b else print("B")
+
+#Test if a is greater than b, AND if c is greater than a:
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
