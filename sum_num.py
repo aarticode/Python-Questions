9510df77d4b9475f68ca85e6f4ea691f72ecf597
aarticode <aartisingh1117@gmail.com>
2023-09-11 20:34:12 +0530

    division

--- sum_num.py
+++ sum_num.py
@@ -1,1 +1,23 @@
-print(3-2)
+ #Why use else in try/except construct in Python?
+#`try:` and `except:` are commonly known for exceptional handling in Python, so where does `else:` come in handy? `else:` will be triggered when no exception is raised
+try:
+    num1 = int(input('Enter Numerator: '))
+    num2 = int(input('Enter Denominator: '))
+    division = num1/num2
+    print(f'Result is: {division}')
+except:
+    print('Invalid input!')
+else:
+    print('Division is successful.')
+
+
+## Try 1 ##
+# Enter Numerator: 2
+# Enter Denominator: d
+# Invalid input!
+
+## Try 2 ##
+# Enter Numerator: 2
+# Enter Denominator: 1
+# Result is: 2.0
+# Division is successful.
