6c1b7120d79f5c7d1420156f433269d27c19c476
aarticode <aartisingh1117@gmail.com>
2023-09-12 21:09:33 +0530

    sum

--- sum_num.py
+++ sum_num.py
@@ -1,4 +1,4 @@
- #Why use else in try/except construct in Python?
+1. #Why use else in try/except construct in Python?
 #`try:` and `except:` are commonly known for exceptional handling in Python, so where does `else:` come in handy? `else:` will be triggered when no exception is raised
 try:
     num1 = int(input('Enter Numerator: '))
@@ -21,3 +21,14 @@ else:
 # Enter Denominator: 1
 # Result is: 2.0
 # Division is successful.
+
+
+
+2.#Write a Python program to calculate the sum of a list of numbers?
+def sum(num):
+    if len(num) == 1:
+        return num[0]  # With only one element in the list, the sum result will be equal to the element.
+    else:
+        return num[0] + sum(num[1:])
+
+print(sum([2, 4, 5, 6, 7]))
