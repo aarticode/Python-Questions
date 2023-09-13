0697a672510e0c6c43ec1a60b60eb8384169d7bf
aarticode <aartisingh1117@gmail.com>
2023-09-13 22:56:34 +0530

    relational_operators

--- operators.py
+++ operators.py
@@ -9,3 +9,13 @@ print(12-23)
 print(12*23)
 # Dividing two values
 print(12/23)
+
+#Relational Operators are used to comparing the values.
+# These operators test the conditions and then returns a boolean value either True or False.
+
+# Examples of Relational Operators
+a, b = 10, 12
+print(a==b) # False
+print(a<b) # True
+print(a<=b) # True
+print(a!=b) # True
