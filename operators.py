e0ba3d85430771884bc0c39db33896cce719b8d8
aarticode <aartisingh1117@gmail.com>
2023-09-13 22:54:34 +0530

    arithmetic_operator

---
+++ operators.py
@@ -0,0 +1,11 @@
+#Arithmetic operators perform basic arithmetic operations.
+# For example "+" is used to add and "?" is used for subtraction.
+
+# Adding two values
+print(12+23)
+# Subtracting two values
+print(12-23)
+# Multiplying two values
+print(12*23)
+# Dividing two values
+print(12/23)
