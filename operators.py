c254dba9f52de917b2b939431753e26ffc7b5d27
aarticode <aartisingh1117@gmail.com>
2023-09-14 19:52:57 +0530

    assigment_operator

--- operators.py
+++ operators.py
@@ -1,4 +1,4 @@
-#Arithmetic operators perform basic arithmetic operations.
+1.#Arithmetic operators perform basic arithmetic operations.
 # For example "+" is used to add and "?" is used for subtraction.
 
 # Adding two values
@@ -10,7 +10,7 @@ print(12*23)
 # Dividing two values
 print(12/23)
 
-#Relational Operators are used to comparing the values.
+2.#Relational Operators are used to comparing the values.
 # These operators test the conditions and then returns a boolean value either True or False.
 
 # Examples of Relational Operators
@@ -19,3 +19,21 @@ print(a==b) # False
 print(a<b) # True
 print(a<=b) # True
 print(a!=b) # True
+
+3.#Assignment operators are used to assigning values to the variables. See the examples below.
+
+
+# Examples of Assignment operators
+a=12
+print(a) # 12
+a += 2
+print(a) # 14
+a -= 2
+print(a) # 12
+a *=2
+print(a) # 24
+a **=2
+print(a) # 576
+
+
+
