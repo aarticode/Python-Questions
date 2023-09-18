5844eafa5fb91a12420614434afc7bbf3845aa5b
aarticode <aartisingh1117@gmail.com>
2023-09-18 22:01:53 +0530

    loops

---
+++ loops_in_python.py
@@ -0,0 +1,16 @@
+# For loop:
+#It is used to repeat a block of code a specific number of times or over a collection of items.
+
+
+
+for i in range(5):
+    print("Hello!")
+
+#While Loop:
+#while loop is used to repeat a block of code as long as a certain condition is true.
+
+count = 0
+
+while count < 5:
+    print("Hello!")
+    count += 1
