a69ee008146bb4763be0643ee60310e3a1c0ae92
aarticode <aartisingh1117@gmail.com>
2023-09-21 21:51:04 +0530

    decorator

--- decorator_func.py
+++ decorator_func.py
@@ -16,5 +16,19 @@ decorator_show = decorator_func(show)
 # Call the decorated show function
 decorator_show()
 
+def decorator_lowercase(function):   # defining a Python decorator
+    def wrapper():
+        result = function()
+        result_lowercase = result.lower()
+        return result_lowercase
+    return wrapper
+@decorator_lowercase ## calling the decorator
+def intro():                     # Normal function
+    return 'Hello, I AM AARTI'
+
+print(intro())
+
+
+
 
 
