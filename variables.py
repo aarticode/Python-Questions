005e4e66b0b606555b5a4867335dac270e003c7a
aarticode <aartisingh1117@gmail.com>
2023-09-25 21:05:18 +0530

    variables

--- variables.py
+++ variables.py
@@ -32,3 +32,18 @@ c=35
 print(a)
 print(b)
 print(c)
+
+
+#In a function, how do you create a global variable?
+#We can create a global variable by designating it as global within every function that assigns to it. We can utilize it in other functions.
+
+global_var = 0
+def modify_global_var():
+ global global_var # Setting global_var as a global variable
+ global_var = 10
+def printing_global_var():
+ print(global_var) # There is no need to declare global variable
+modify_global_var()
+printing_global_var() # Prints 10
+
+
