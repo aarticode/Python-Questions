22dbad2e258c23367ecce18c5ef21f285a50b94d
aarticode <aartisingh1117@gmail.com>
2023-09-26 20:35:09 +0530

    array

---
+++ array.py
@@ -0,0 +1,8 @@
+#In Python, what is a negative index?
+#In Arrays and Lists, Python contains a unique feature called negative indexing. Python starts indexing from the beginning of an array or list in a positive integer but reads items from the ending of an array or list in a negative index.
+
+import array
+a = [4, 6, 8, 3, 1, 7]
+print(a[-3])
+print(a[-5])
+print(a[-1])
