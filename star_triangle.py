9ab1fb6dfdfb7f8a28823bb729a4a9015eedd3dc
aarticode <aartisingh1117@gmail.com>
2023-09-28 20:45:46 +0530

    star_triangle

--- star_triangle.py
+++ star_triangle.py
@@ -14,3 +14,13 @@ for i in range(n):
     for j in range(i + 1):
         print('*', end=' ')
     print()
+
+for i in range(n):
+        for j in range(i, n):
+            print(' ', end=' ')
+        for j in range(i):
+            print('*', end=' ')
+        for j in range(i + 1):
+            print('*', end=' ')
+        print()
+
