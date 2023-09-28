d869e9d0911a030d73ebd8087e328e85b620cd64
aarticode <aartisingh1117@gmail.com>
2023-09-28 20:57:32 +0530

    array

--- array.py
+++ array.py
@@ -6,3 +6,13 @@ a = [4, 6, 8, 3, 1, 7]
 print(a[-3])
 print(a[-5])
 print(a[-1])
+
+
+import array
+a = array.array('i', [1, 2, 3])
+for i in a:
+    print(i, end=' ')    #OUTPUT: 1 2 3
+a = array.array('i', [1, 2, 'string'])    #OUTPUT: TypeError: an integer is required (got type str)
+a = [1, 2, 'string']
+for i in a:
+   print(i, end=' ')
