8afe5199396604f159466e09c1857e61b0199afa
aarticode <aartisingh1117@gmail.com>
2023-09-29 21:39:51 +0530

    prime_number

--- prime_number.py
+++ prime_number.py
@@ -43,3 +43,27 @@ if is_prime(num):
 else:
     print(f"{num} is not a prime number.")
 
+
+#Write a Python program to check prime numbers.
+#Here is the Python program to check whether a number is prime or not:
+# Function to check prime number
+def PrimeChecking(num):
+    # Condition to check given number is more than 1
+    if num > 1:
+        # For look to iterate over the given number
+        for i in range(2, int(num/2) + 1):
+            # Condition to check if the given number is divisible
+            if (num % i) == 0:
+                #If divisible by any number it's not a prime number
+                print("The number ",num, "is not a prime number")
+                break
+        # Else print it as a prime number
+        else:
+            print("The number ",num, "is a prime number")
+    # If the given number is 1
+    else:
+        print("The number ",num, "is not a prime number")
+# Input function to take the number from user
+num = int(input("Enter a number to check prime or not: "))
+# To print the result, whether a given number is prime or not
+PrimeChecking(num)
