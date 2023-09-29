1a2e482838c71d828eb67a97b8bd2131e892737a
aarticode <aartisingh1117@gmail.com>
2023-09-29 21:44:11 +0530

    factorial

--- factorial.py
+++ factorial.py
@@ -7,3 +7,20 @@ def fact(n):
 x=5
 result= fact(x)
 print(result)
+
+# Python program to find factorial of a given number
+
+#importing the math function
+import math
+
+#Defining the factorial() function to find factorial
+def factorial(num):
+	return(math.factorial(num))
+
+
+# Input function to get the number from user
+num = int(input('Please enter a number to find the factorial: '))
+
+#Printing the factorial of the given number
+print("The factorial of the given number", num, "is",
+	factorial(num))
