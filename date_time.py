5b8ffc81f582abd039c5e0fe1975c42cbc4d43e2
aarticode <aartisingh1117@gmail.com>
2023-09-30 22:27:07 +0530

    date_time

--- date_time.py
+++ date_time.py
@@ -38,3 +38,18 @@ print(x.year)
 print(x.strftime("%A"))
 
 
+
+#To create a date, we can use the datetime() class (constructor) of the datetime module.
+
+#The datetime() class requires three parameters to create a date: year, month, day.
+
+#Example
+#Create a date object:
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
