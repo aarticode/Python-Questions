2986afe61542825643af0b6bc0a7de41cc5d025a
aarticode <aartisingh1117@gmail.com>
2023-10-02 15:20:52 +0530

    data_types

---
+++ data_types.py
@@ -0,0 +1,40 @@
+#Python first project realted to python basic that covered all
+# data types of python (list ,strings, numbers, tuples ,dictionary) and loops
+
+# Importing required modules
+import random
+import math
+
+# List
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Strings
+my_string = "Hello, World!"
+print("String:", my_string)
+
+# Numbers
+my_number = 10
+print("Number:", my_number)
+
+# Tuples
+my_tuple = (6, 7, 8)
+print("Tuple:", my_tuple)
+
+# Dictionary
+my_dict = {'name': 'John', 'age': 25}
+print("Dictionary:", my_dict)
+
+# Loops
+print("Printing numbers from 1 to 5 using a loop:")
+for num in range(1, 6):
+    print(num)
+
+# Math operations
+print("Square root of 16:", math.sqrt(16))
+print("Random number between 1 and 10:", random.randint(1, 10))
+
+
+
+
+
