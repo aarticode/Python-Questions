2c7195572fc4723ca675cbbbc9019cf7707ccd63
aarticode <aartisingh1117@gmail.com>
2023-10-02 15:30:13 +0530

    mymodule

---
+++ my_module.py
@@ -0,0 +1,17 @@
+#What is a Module?
+#Consider a module to be the same as a code library.
+
+#A file containing a set of functions you want to include in your application.
+
+#Create a Module
+#To create a module just save the code you want in a file with the file extension .py:
+def greeting(name):
+  print("Hello, " + name)
+
+#Import the module named mymodule, and call the greeting function:
+
+import my_module
+
+import my_module
+
+my_module.greeting("Aarti")
