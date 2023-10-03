9392c1a58db00c70d75f68ca9f3d5235c688cfdb
aarticode <aartisingh1117@gmail.com>
2023-10-03 21:37:06 +0530

    solve

--- solve2.py
+++ solve2.py
@@ -12,3 +12,15 @@ result = reverse_sentence(sentence)
 print(result)
 
 
+class Calculator:
+    def add(self, *args):
+        return sum(args)
+
+# Create an object
+calc = Calculator()
+
+# Call the add method with different numbers of arguments
+print(calc.add(2, 3))      # Output: 5
+print(calc.add(2, 3, 4))   # Output: 9
+
+
