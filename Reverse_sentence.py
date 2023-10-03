1be82a1bd531b439569521115a499735ac30b6eb
aarticode <aartisingh1117@gmail.com>
2023-10-03 21:41:27 +0530

    result

--- Reverse_sentence.py
+++ Reverse_sentence.py
@@ -9,4 +9,17 @@ sentence = "Hello world! Python is awesome."
 result = reverse_sentence(sentence)
 print(result)
 
+#Question: Write a Python function to remove duplicate elements from a given list.
+
+def remove_duplicates(lst):
+    return list(set(lst))
+
+# Test the function
+numbers = [1, 2, 3, 3, 4, 4, 5, 6, 6]
+result = remove_duplicates(numbers)
+print(result)
+
+
+
+
 
