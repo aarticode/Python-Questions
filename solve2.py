94981677c4cea4b22f65cadd6980220ba3338f5a
aarticode <aartisingh1117@gmail.com>
2023-10-04 20:44:05 +0530

    array

--- solve2.py
+++ solve2.py
@@ -36,4 +36,18 @@ my_list = [1, 2, 3, 4, 5]
 length = len(my_list)
 print(length)
 
+# How to remove values to a python array?
+#Elements can be removed from a python array by using pop() or remove() methods.
+
+#pop(): This function will return the removed element .
+
+#remove():It will not return the removed element
+import array
+
+x = array.array('d', [8.1, 2.4, 6.8, 1.1, 7.7, 1.2, 3.6])
+print(x.pop())
+print(x.pop(3))
+x.remove(8.1)
+print(x)
+
 
