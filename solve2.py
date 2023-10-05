ab202f223cd3a7c43217a6fa0799b7358a7f1439
aarticode <aartisingh1117@gmail.com>
2023-10-05 22:19:43 +0530

    if_else

--- solve2.py
+++ solve2.py
@@ -51,3 +51,17 @@ x.remove(8.1)
 print(x)
 
 
+#Python if else Statement:
+
+def main():
+    x, y = 2, 8
+
+    if (x < y):
+        st = "x is less than y"
+    print(st)
+
+
+if __name__ == "__main__":
+    main()
+
+
