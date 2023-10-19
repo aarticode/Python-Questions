e011ee10e4edd6b33a11e13f365fbd91b776b33f
aarticode <aartisingh1117@gmail.com>
2023-10-19 11:48:23 +0530

    fibonacci_series

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -6,3 +6,15 @@ for i in range(5):
 # Converting the list of integers to string
 print(', '.join(str(e) for e in fib))
 
+
+# Writing Fibonacci Series
+
+
+fib = [0,1]
+# Range starts from 0 by default
+for i in range(6):
+    fib.append(fib[-1] + fib[-2])
+
+# Converting the list of integers to string
+print(', '.join(str(e) for e in fib))
+
