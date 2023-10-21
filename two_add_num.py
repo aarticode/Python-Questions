e226e52c60dd7f098fdc2019f62fa8fd75d82db1
aarticode <aartisingh1117@gmail.com>
2023-10-21 22:05:19 +0530

    list

--- two_add_num.py
+++ two_add_num.py
@@ -4,3 +4,13 @@ print(11*2)
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(numbers[1 : : 2])
+
+
+
+lst1 = [1, 2, 3]
+lst2 = [4, 5, 6]
+
+res_lst = []
+for i in range(0, len(lst1)):
+    res_lst.append(lst1[i] + lst2[i])
+print(res_lst)
