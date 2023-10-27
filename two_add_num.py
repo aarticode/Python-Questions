1d9d01da57a9610bfbbf82c251c02b97a3f72339
aarticode <aartisingh1117@gmail.com>
2023-10-27 20:07:23 +0530

    prime

--- two_add_num.py
+++ two_add_num.py
@@ -10,7 +10,25 @@ print(numbers[1 : : 2])
 lst1 = [1, 2, 3]
 lst2 = [4, 5, 6]
 
-res_lst = []
-for i in range(0, len(lst1)):
-    res_lst.append(lst1[i] + lst2[i])
-print(res_lst)
+
+def is_prime(number):
+    if number <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+
+    # Check for factors from 2 to the square root of the number
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False  # Found a factor, the number is not prime
+
+    return True  # No factors found, the number is prime
+
+# Test cases
+print(is_prime(2))      # True (2 is prime)
+print(is_prime(17))     # True (17 is prime)
+print(is_prime(9))      # False (9 is not prime)
+print(is_prime(1))      # False (1 is not prime)
+print(is_prime(25))     # False (25 is not prime)
+
+
+
+
