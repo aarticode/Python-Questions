c7fa55d4d4dcf36fac73b6391f6948a54d99ac09
aarticode <aartisingh1117@gmail.com>
2023-10-28 22:38:23 +0530

    calender

--- calender.py
+++ calender.py
@@ -8,3 +8,12 @@ mm = 12
 
 # To display the calendar for a specific year and month
 print(calendar.month(yy, mm))
+
+# import module
+import calendar
+
+yy = 2023
+mm = 4
+
+# display the calendar
+print(calendar.month(yy, mm))
