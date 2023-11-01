3dbe46ba66a96a05b3ab1e25bd1a5a9eaf94c860
aarticode <aartisingh1117@gmail.com>
2023-11-01 23:03:25 +0530

    func

--- solve2.py
+++ solve2.py
@@ -148,6 +148,23 @@ for character in word:
         count += 1
 print(count)
 
+#Function Inside Function
+#As explained in the example above, the variable x is not available outside the function, but it is available for any function inside the function:
+
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+
+
+
+
+
+
 
 
 
